4dcf0e8483d4f97dfcacf7bbee4f5426529fd8d7
vendredl <node.js_@kakao.com>
2023-08-27 23:23:11 +0900

    :lipstick: 헤더 레이아웃 생성

---
+++ src/pages/layout/Layout.tsx
@@ -0,0 +1,19 @@
+// components/Layout.tsx
+import React from 'react';
+import Header from './Header';
+import Upload from './Upload';
+
+type LayoutProps = {
+    children: React.ReactNode;
+}
+
+function Layout({ children }: LayoutProps) {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
