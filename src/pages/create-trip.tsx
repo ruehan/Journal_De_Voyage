e8af94b0aebbf6208739e59abf7e9983b9e2ca74
vendredl <node.js_@kakao.com>
2023-09-05 00:05:31 +0900

    :lipstick: 데이터 입력 가능하도록 폼 생성

--- src/pages/create-trip.tsx
+++ src/pages/create-trip.tsx
@@ -1,40 +1,50 @@
-// components/ImageUploadForm.tsx
 import { useForm } from 'react-hook-form';
 
 type FormData = {
+  title: string;
+  content: string;
   image: FileList;
 };
 
 const ImageUploadForm: React.FC = () => {
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const {register, handleSubmit} = useForm<FormData>();
+
+  const onSubmit = async (data: FormData) => {
     const formData = new FormData();
-    const title = (e.currentTarget.title as HTMLInputElement).value;
-    const content = (e.currentTarget.content as HTMLInputElement).value;
-    const file = (e.currentTarget.file as HTMLInputElement).files?.[0];
+    formData.append('title', data.title);
+    formData.append('content', data.content);
+    formData.append('image', data.image[0]);
 
-    formData.append('title', title)
-    formData.append('content', content)
-    formData.append('file', file as Blob)
+    console.log(formData)
 
-    const res = await fetch('/api/upload', {
+    const response = await fetch('/api/upload', {
       method: 'POST',
-      body: formData,
+      body: formData
     })
 
-    if(res.ok){
-      alert('Uploaded successfully')
+    const result = await response.json();
+
+    if(response.ok){
+      alert('Upload Success');
     }else{
-      alert('Upload failed')
+      alert('Upload Fail');
     }
   }
 
   return (
-    <form onSubmit={handleSubmit} className="w-full rounded-xl bg-white relative flex flex-col items-center justify-center text-gray-400 p-4">
-      <input type="text" placeholder="간단한 설명" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4"/>  
-      <input type="text" placeholder="자세한 내용" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4" />
-      <input type="text" placeholder="경험 또는 느낀점" className="w-4/6 border-2 rounded-xl border-gray-200 h-36 m-4"></input>
-      <input type="file" className="w-4/6 border-2 rounded-xl border-gray-200 m-4" />
+    // <form className="w-full rounded-xl bg-white relative flex flex-col items-center justify-center text-gray-400 p-4">
+    //   <input type="text" placeholder="간단한 설명" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4"/>  
+    //   <input type="text" placeholder="자세한 내용" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4" />
+    //   <input type="text" placeholder="경험 또는 느낀점" className="w-4/6 border-2 rounded-xl border-gray-200 h-36 m-4"></input>
+    //   <input type="file" className="w-4/6 border-2 rounded-xl border-gray-200 m-4" />
+    //   <button type="submit" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4">Upload</button>
+    // </form>
+
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full rounded-xl bg-white relative flex flex-col items-center justify-center text-gray-400 p-4">
+      <input {...register('title')} placeholder="Title" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4"/>
+      <input {...register('content')} placeholder="Content" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4"/>
+      <input {...register('image')} type="file" className="w-4/6 border-2 rounded-xl border-gray-200 m-4"/>
       <button type="submit" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4">Upload</button>
     </form>
 );
