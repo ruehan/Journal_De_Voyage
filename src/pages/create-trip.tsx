2f849f77c61cc120f55d35da456e981c394c2548
vendredl <node.js_@kakao.com>
2023-09-07 16:56:24 +0900

    :lipstick: 앨범 선택 및 추가 가능하도록 UI 추가

--- src/pages/create-trip.tsx
+++ src/pages/create-trip.tsx
@@ -1,9 +1,10 @@
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 
 type FormData = {
   title: string;
   content: string;
   images: FileList;
+  album: string;
 };
 
 const ImageUploadForm: React.FC = () => {
@@ -36,15 +37,19 @@ const ImageUploadForm: React.FC = () => {
   }
 
   return (
-    // <form className="w-full rounded-xl bg-white relative flex flex-col items-center justify-center text-gray-400 p-4">
-    //   <input type="text" placeholder="간단한 설명" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4"/>  
-    //   <input type="text" placeholder="자세한 내용" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4" />
-    //   <input type="text" placeholder="경험 또는 느낀점" className="w-4/6 border-2 rounded-xl border-gray-200 h-36 m-4"></input>
-    //   <input type="file" className="w-4/6 border-2 rounded-xl border-gray-200 m-4" />
-    //   <button type="submit" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4">Upload</button>
-    // </form>
-
     <form onSubmit={handleSubmit(onSubmit)} className="w-full rounded-xl bg-white relative flex flex-col items-center justify-center text-gray-400 p-4">
+      <div className="flex w-4/6 items-center justify-center">
+        <select {...register('album')}
+        className="border-2 rounded-xl border-gray-200 h-12 w-5/6">
+          <option value="" disabled>
+            Select your Album
+          </option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+        <div className="bg-green-100 h-12 w-12 rounded-full text-3xl ml-4 flex items-center justify-center">+</div>
+      </div>
       <input {...register('title')} placeholder="Title" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4"/>
       <input {...register('content')} placeholder="Content" className="w-4/6 border-2 rounded-xl border-gray-200 h-12 m-4"/>
       <input {...register('images')} type="file" multiple className="w-4/6 border-2 rounded-xl border-gray-200 m-4"/>
